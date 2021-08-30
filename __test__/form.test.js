916a30a270d640cc612305b69abe18c778bbf380
David C. Page <davidpage876@gmail.com>
2021-08-30 11:01:50 +1000

    feat: validateDestination() passes on good input

--- __test__/form.test.js
+++ __test__/form.test.js
@@ -4,6 +4,11 @@ describe("Testing form validation", () => {
     test("validateDestination() exists", () => {
         expect(validateDestination).toBeDefined();
     })
+    test("validateDestination() on good input", () => {
+        expect(() => validateDestination('Tokyo')).not.toThrow(Error);
+        expect(() => validateDestination('San Francisco')).not.toThrow(Error);
+        expect(() => validateDestination('London')).not.toThrow(Error);
+    })
     test("validateDestination() on blank input", () => {
         expect(() => validateDestination('')).toThrow(Error);
         expect(() => validateDestination(' ')).toThrow(Error);
--- src/client/js/form.js
+++ src/client/js/form.js
@@ -8,7 +8,9 @@
  * @throws {Error} Thrown if validation failed.
  */
 function validateDestination(dest) {
-    throw new Error('Destination cannot be blank');
+    if (!dest.trim()) {
+        throw new Error('Destination cannot be blank');
+    }
 }
 
 /**
