5d194e702b3fd590f0bb71a448d22a4474d123c9
David C. Page <davidpage876@gmail.com>
2021-08-30 10:28:04 +1000

    feat: Ensure date input cannot be earlier than today

--- src/client/index.js
+++ src/client/index.js
@@ -9,4 +9,11 @@ import './styles/main.scss';
 
 hello();
 
+// Ensure date input cannot be earlier than today.
+(() => {
+    const inputForm = document.getElementById('input-form');
+    const dateInput = inputForm.date;
+    dateInput.min = new Date().toISOString().slice(0, 10);
+})();
+
 export { buildEnv }
