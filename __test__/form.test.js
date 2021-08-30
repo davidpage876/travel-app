bd8913cab7fe2508d2c02cb22e86c5b4c96a5f64
David C. Page <davidpage876@gmail.com>
2021-08-30 11:13:16 +1000

    style: Use semicolons consistently

--- __test__/form.test.js
+++ __test__/form.test.js
@@ -3,19 +3,19 @@ import { validateDestination, validateDate } from "../src/client/js/form";
 describe("Testing form validation", () => {
     test("validateDestination() exists", () => {
         expect(validateDestination).toBeDefined();
-    })
+    });
     test("validateDestination() on good input", () => {
         expect(validateDestination('Tokyo')).toBe('Tokyo');
         expect(validateDestination('San Francisco')).toBe('San Francisco');
         expect(validateDestination(' London  ')).toBe('London');
-    })
+    });
     test("validateDestination() on blank input", () => {
         expect(() => validateDestination('')).toThrow(Error);
         expect(() => validateDestination(' ')).toThrow(Error);
         expect(() => validateDestination('    ')).toThrow(Error);
-    })
+    });
 
     test("validateDate() exists", () => {
         expect(validateDate).toBeDefined();
-    })
+    });
 });
