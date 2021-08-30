cec144f74d79fb355d0461e4d22d972767a05bc4
David C. Page <davidpage876@gmail.com>
2021-08-30 11:13:57 +1000

    style: Use quotes consistently

--- __test__/form.test.js
+++ __test__/form.test.js
@@ -1,21 +1,21 @@
 import { validateDestination, validateDate } from "../src/client/js/form";
 
 describe("Testing form validation", () => {
-    test("validateDestination() exists", () => {
+    test('validateDestination() exists', () => {
         expect(validateDestination).toBeDefined();
     });
-    test("validateDestination() on good input", () => {
+    test('validateDestination() on good input', () => {
         expect(validateDestination('Tokyo')).toBe('Tokyo');
         expect(validateDestination('San Francisco')).toBe('San Francisco');
         expect(validateDestination(' London  ')).toBe('London');
     });
-    test("validateDestination() on blank input", () => {
+    test('validateDestination() on blank input', () => {
         expect(() => validateDestination('')).toThrow(Error);
         expect(() => validateDestination(' ')).toThrow(Error);
         expect(() => validateDestination('    ')).toThrow(Error);
     });
 
-    test("validateDate() exists", () => {
+    test('validateDate() exists', () => {
         expect(validateDate).toBeDefined();
     });
 });
