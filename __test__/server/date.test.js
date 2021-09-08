f4339ea9a9509a3f277e5524e15e9eb9b3490ddc
David C. Page <davidpage876@gmail.com>
2021-09-08 12:04:00 +1000

    feat: calculateDaysDelta() with days before

--- __test__/server/date.test.js
+++ __test__/server/date.test.js
@@ -13,4 +13,10 @@ describe('Testing date utility function', () => {
         expect(calculateDaysDelta(new Date('01/01/2020'), new Date('01/04/2020'))).toBe(3);
         expect(calculateDaysDelta(new Date('01/01/2020'), new Date('02/01/2020'))).toBe(31);
     });
+    test('calculateDaysDelta() with days before', () => {
+        expect(calculateDaysDelta(new Date('01/02/2020'), new Date('01/01/2020'))).toBe(-1);
+        expect(calculateDaysDelta(new Date('01/03/2020'), new Date('01/01/2020'))).toBe(-2);
+        expect(calculateDaysDelta(new Date('01/04/2020'), new Date('01/01/2020'))).toBe(-3);
+        expect(calculateDaysDelta(new Date('02/01/2020'), new Date('01/01/2020'))).toBe(-31);
+    });
 });
